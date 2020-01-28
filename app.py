477763c7b4c60fd9cde7a6dcd033910647d62d6d
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-01-28 10:21:01 -0700

    GET all guides and fixed dataerror

--- app.py
+++ app.py
@@ -40,6 +40,12 @@ def add_guide():
     guide = Guide.query.get(new_guide.id)
     return guide_schema.jsonify(guide)
 
+# Endpoint to query all guids
+@app.route("/guides", methods=["GET"])
+def get_guides():
+    all_guides = Guide.query.all()
+    result = guides_schema.dump(all_guides)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
